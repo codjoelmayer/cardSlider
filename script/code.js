8d96ed3582443cd160435f742387c8400e55ef80
codjoelmayer <tmjoelm4@gmail.com>
2024-05-30 12:15:15 +0200

    Done

--- script/code.js
+++ script/code.js
@@ -1,1 +1,10 @@
-// 
+let slider = document.querySelector('.slider')
+let firstChildWidth = document.querySelector('.slider :first-child')
+let prevItem = document.querySelector('[prevItem]')
+let nextItem = document.querySelector('[nextItem]')
+prevItem.addEventListener('click', () => {
+    slider.scrollLeft -= firstChildWidth.offsetWidth
+})
+nextItem.addEventListener('click', () => {
+    slider.scrollLeft += firstChildWidth.offsetWidth + 2
+})
